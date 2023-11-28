e88b3f5ab3d1c5ca7d40ecac1ac32e9c80e882e5
Daniel <danya.ostap@gmail.com>
2023-11-28 20:40:37 +0200

    Add keygen

--- internal/ecdsa/ecdsa.go
+++ internal/ecdsa/ecdsa.go
@@ -2,17 +2,44 @@ package ecdsa
 
 import (
 	"crypto/elliptic"
+	"crypto/rand"
+	"math/big"
 	"sync"
 
 	"github.com/danielost/ecpoint-wrappers/pkg/ecwrapper"
 )
 
-var once *sync.Once
+var once = &sync.Once{}
 var ecw *ecwrapper.ECWrapper
+
+type KeyPair struct {
+	privateKey *big.Int
+	publicKey  *ecwrapper.ECPoint
+}
+
+func (kp *KeyPair) PublicKey() *ecwrapper.ECPoint {
+	return kp.publicKey
+}
+
+func (kp *KeyPair) PrivateKey() *big.Int {
+	return kp.privateKey
+}
 
 func getECWrapper() *ecwrapper.ECWrapper {
 	once.Do(func() {
 		ecw = ecwrapper.NewECWrapper(elliptic.P256())
 	})
 	return ecw
+}
+
+func GenerateKeys() (*KeyPair, error) {
+	curve := getECWrapper()
+	N := curve.Params().N
+	G := curve.GetBasePointG()
+	sKey, err := rand.Int(rand.Reader, N)
+	if err != nil {
+		return nil, err
+	}
+	pKey := curve.ScalarMult(sKey, G)
+	return &KeyPair{sKey, pKey}, nil
 }
